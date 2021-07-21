c18c4d4854df321d543b487e75e0c1fd081b1c16
rafaelHSnascimento <rafael.hs.nascimento@gmail.com>
2021-07-21 12:01:41 -0300

    comentarios removidos do fonte

--- operacoes-logicas.js
+++ operacoes-logicas.js
@@ -1,21 +1,3 @@
-/*passou <- falso
-escreval ("Digite o nome do aluno: ")
-leia (nome)
-escreval ("Digite a nota 1 do aluno: ")
-leia(nota1)
-escreval ("Digite a nota 2 do aluno: ")
-leia (nota2)
-media <- (nota1+nota2) / 2
-se  media >= 50 então
-passou := verdadeiro
-fimse
-
-se (passou) && (media >= 50 || media <= 70) entao
-escreval (nome, " Foi Aprovado! ")
-senao
-  escreval (nome, "Foi Reprovado! ")
-fimse*/
-
 var nome, nota1, nota2, passou;
 passou = false
 nome = prompt("Digite o nome do aluno: ")
